5d2e15adb525938cf5ab50b2a669de04ee9ee677
Adhvaith Prasad <83104991+firescrypt@users.noreply.github.com>
2022-01-24 12:26:18 +0000

    solved eslint errors

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const express = require('express');
 const app = express();
 const Server = require('node-git-server');
 
+const __dirname = path.resolve();
+
 const repos = new Server(path.resolve(__dirname, 'tmp'), {
     autoCreate: true
 });
 
-const port = process.env.PORT || 7005;
+const port = 7005;
 
 app.use('/console', express.static(__dirname + '/console'));
 
@@ -15,6 +17,6 @@ app.use('/git', function(req, res) {
   repos.handle(req, res)
 });
 
-app.listen(7005, () => {
+app.listen(port, () => {
   console.log(`Express http server listening`);
 });

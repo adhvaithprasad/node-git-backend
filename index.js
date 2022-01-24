64cc93ca3d7afe8cb7db876d245b58a739e05d5b
AdhvaithPrasad <>
2022-01-24 12:42:41 +0000

    added firebase

--- index.js
+++ index.js
@@ -2,7 +2,19 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const Server = require('node-git-server');
+var admin = require("firebase-admin");
+const { exec } = require('child_process');
+var serviceAccount = require("./tribble.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://tribble-66bad-default-rtdb.asia-southeast1.firebasedatabase.app",
+  storageBucket: "gs://tribble-66bad.appspot.com"
+});
+var Git = require("nodegit");
+const { getDatabase } = require('firebase-admin/database');
 
+const db = getDatabase();
+var ref = db.ref('/');
 const __dirname = path.resolve();
 
 const repos = new Server(path.resolve(__dirname, 'tmp'), {

4cf9a1eec8fe0fa3811a1e267ab17301308a24cf
Adhvaith Prasad <83104991+firescrypt@users.noreply.github.com>
2022-03-05 14:57:49 +0000

    significant changes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const Server = require('node-git-server');
 var admin = require("firebase-admin");
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 var serviceAccount = require("./tribble.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -15,7 +16,7 @@ const { getDatabase } = require('firebase-admin/database');
 
 const db = getDatabase();
 let ref = db.ref('/');
-const dirTree = require('directory-tree');
+
 
 const repos = new Server(path.resolve(__dirname, 'tmp'), {
   autoCreate: true
@@ -25,101 +26,85 @@ var getDirectories = function (src, callback) {
   glob(src + '/**/*', callback);
 };
 const port = 7005;
-
-repos.on('push', (push) => {
-
-
-  push.accept();
-  
 function replaceAll(str, find, replace) {
                 var escapedFind = find.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
                 return str.replace(new RegExp(escapedFind, 'g'), replace);
-              }
-const convert = require('amrhextotext');
-  var clone_link = "https://jk.adhvaithprasad.repl.co/git/" + push.repo;
-  ref = db.ref('/'+push.repo + '/' + push.branch + '/'+push.commit);
-      var folder = "./repos/"+push.repo;
-  Git.Clone(clone_link, "./repos/"+push.repo)
-  .then(function(){
-
-    const filteredTree = dirTree('./repos/'+push.repo, {attributes:['type']});
-
-
-const hextree = JSON.stringify(filteredTree);
-var reponame = replaceAll(push.repo,".","--dot--")
-ref.child("treeview").child(reponame).set({
-                treeview:hextree
-              });
-
-  })
-    .then(function recursive() {
-
-      getDirectories(folder, function(err, res) {
-        if (err) {
-          console.log('Error', err);
-        } else {
-
-          res.forEach(element => {
-
-            function isDir(path) {
-              try {
-                var stat = fs.lstatSync(path);
-                return stat.isDirectory();
-              } catch (e) {
-                // lstatSync throws an error if path doesn't exist
-                return false;
-              }
-            }
-
-            var mnk = isDir(element);
-            if (mnk === true) {
-              // falsy
-           
-            }
-            else {
-
-              
-              var kElement = replaceAll(element, "./repos/"+push.repo, "")
-              var mElement = replaceAll(kElement, ".", "--dot--");
-
-             
-              var filevalue = convert.textToHex(fs.readFileSync(element).toString());
-              ref.child("storage").child(mElement).set({
-                value: filevalue
-              });
-              
-
-            }
-
-          })
-
+}
+  const storageRef = admin.storage().bucket(`gs://tribble-66bad.appspot.com`);
+ async function uploadFile(path, filename) {
+    const storage = await storageRef.upload(path, {
+        public: true,
+        destination: `uploads/raw/${filename}`,
+        metadata: {
+            firebaseStorageDownloadTokens: uuidv4(),
         }
-      });
-
-    })
-    
-
-
-
-    .catch(err => {
-      console.error(err);
     });
 
 
+    return storage[0].metadata.mediaLink;
+}
 
+var walk = function(dir, done) {
+  var results = [];
+  fs.readdir(dir, function(err, list) {
+    if (err) return done(err);
+    var i = 0;
+    (function next() {
+      var file = list[i++];
+      if (!file) return done(null, results);
+      file = path.resolve(dir, file);
+      fs.stat(file, function(err, stat) {
+        if (stat && stat.isDirectory()) {
+          walk(file, function(err, res) {
+            results = results.concat(res);
+            next();
+          });
+        } else {
+          results.push(file);
+          next();
+        }
+      });
+    })();
+  });
+}
 
+repos.on('push', (push) => {
+push.accept();
+const axios = require("axios");
+  var url = "http://git.krios.studio/" + push.repo;
+  var user = push.repo.split("/")[0];
+  var name = push.repo.split("/")[1];
+const options = {
+  method: 'GET',
+  url: 'https://api.krios.studio/clone_repo',
+  params: {
+    git_url: url,
+    repo_name: name,
+    user: user
+  },
+  headers: {'content-type': 'application/json'}
+};
+
+axios.request(options).then(function (response) {
+  console.log(response.data);
+}).catch(function (error) {
+  console.error(error);
+});
+  
 });
 
+
+
+const cert = fs.readFileSync('./path/to/the/cert.crt');
+const ca = fs.readFileSync('./path/to/the/ca.crt');
+const key = fs.readFileSync('./path/to/the/private.key');
+
+
+
+  
 repos.on('fetch', (fetch)=>{
   fetch.accept();
 })
-
-
-
-
-
-
-
 
 var useragent = require('express-useragent');
  
@@ -130,10 +115,16 @@ app.use('/', function(req, res) {
 repos.handle(req, res)
   }
   else{
-    res.send('hi')
+    fs.createReadStream('index.html').pipe(res)
   }
 });
 
+// (async() => {
+//    const file = await uploadFile('./README.md', "README.md");
+//    console.log(file);
+// })();
+
+  
 app.listen(port, () => {
 //
 });
